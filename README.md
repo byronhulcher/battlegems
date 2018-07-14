e5b4ee2fc4a16b1cf2a4e4d52d504ca96b5223f5
Glitch (twitter-image-downloader) <none>
2018-07-14 16:09:27 +0000

    🐬🕋 Bot can download urls of all photo urls from a twitter account

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# battlegems
+Generate an e-books style twitter bot using your own tweets!
+
+How to build your own bot:
+- Click the settings above and "Remix" this project!
+- Get your [Twitter OAuth tokens](http://botwiki.org/tutorials/how-to-create-a-twitter-app ) for this app and add them to `.env`
+
+
+🤖 [Byron Hulcher](http://twitter.com/hypirlink)
---
+++ cmd/clean-data.js
@@ -0,0 +1,31 @@
+const del = require('del'),
+  storage = require('node-persist');
+
+async function deleteStorageData() {
+  await storage.init({
+    dir: './.data/storage'
+  });
+  await storage.clear();
+}
+
+async function deleteFileData() {
+  await del('.data/*.txt');
+}
+
+function inFiveSeconds() {
+  return new Promise( (resolve) => {
+    setTimeout(function() {
+      resolve();
+    }, 5000);
+  });
+}
+
+async function main(){
+  console.log("Clearing data in 5 seconds...")
+  await inFiveSeconds();
+  await deleteFileData();
+  await deleteStorageData();
+  console.log("Data is cleared!");
+}
+
+main();
