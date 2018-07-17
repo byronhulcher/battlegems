fc7643328b9cd3c5b81a1781ceba369e15a739e6
Glitch (twitter-image-downloader) <none>
2018-07-17 02:05:42 +0000

    ☄️👘 Tweets tracked w/ timestamp, now serving data from /media/ endpoint

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-      storage = require('./lib/storage');
+      mediaStorage = require('./lib/media');
 
 let app = express(),
   listener;
@@ -7,8 +7,13 @@ let app = express(),
 app.use(express.static('public')); // serve static files like index.html http://expressjs.com/en/starter/static-files.html
 app.use(express.json());
 
-app.get('/', async function (req, res) {
-  let media = await storage.getItem(`${process.env.ACCOUNT_TO_DOWNLOAD}:tweet_media`) || [];
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response, next) {
+  response.sendFile(__dirname + '/pages/index.html');
+});
+
+app.get('/media/', async function (req, res) {
+  let media = await mediaStorage.getAllCached(process.env.TWITTER_ACCOUNT_TO_DOWNLOAD_FROM)
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(media));
 });
