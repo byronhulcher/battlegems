b0a987c1879aa1260e4ccd40065ef4648939a536
Glitch (twitter-image-downloader) <none>
2018-07-17 01:10:42 +0000

    💔🍡 Updated with Glitch

--- app.js
+++ app.js
@@ -7,21 +7,10 @@ let app = express(),
 app.use(express.static('public')); // serve static files like index.html http://expressjs.com/en/starter/static-files.html
 app.use(express.json());
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response, next) {
-  response.sendFile(__dirname + '/pages/index.html');
-});
-
-app.get('/media/', async function (req, res) {
+app.get('/', async function (req, res) {
   let media = await storage.getItem(`${process.env.ACCOUNT_TO_DOWNLOAD}:tweet_media`) || [];
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(media.map(item => {
-    return {
-      s3Url: item.s3Url,
-      tweetMediaUrl: item.tweetMediaUrl,
-      tweetId: item.tweetId
-    }
-  })));
+  res.send(JSON.stringify(media));
 });
 
 // listen for requests :)
